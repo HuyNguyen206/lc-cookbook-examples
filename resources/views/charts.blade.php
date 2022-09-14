32497b6efbef84915deb5eb28ab6b5367c9d7f95
huynguyen <nguyenlehuyuit@gmail.com>
2022-09-14 17:59:15 +0700

    Eps 1 - In progress

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -1,10 +1,45 @@
 <x-app-layout>
-    <div class="bg-white rounded-md border my-8 px-6 py-6">
+    <div class="bg-white rounded-md border my-8 px-6 py-6 mx-40">
         <div>
             <h2 class="text-2xl font-semibold">Charts</h2>
             <div class="mt-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, doloremque eveniet architecto culpa similique iure aspernatur nulla at, impedit veniam a ducimus non officiis quam perferendis, dolore sint maxime officia.
+                <canvas id="myChart"></canvas>
             </div>
         </div>
     </div>
+    @push('scripts')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            const labels = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+            ];
+
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            };
+
+            const config = {
+                type: 'bar',
+                data: data,
+                options: {
+                }
+            };
+
+            const myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+        </script>
+    @endpush
 </x-app-layout>
