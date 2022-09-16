117af46e2f9ed6d619db9fc275589f6b81dca95d
huynguyen <nguyenlehuyuit@gmail.com>
2022-09-16 10:21:29 +0700

    Eps 3 - Stats Cards  => First way refactor to reduce the same code

---
+++ resources/views/stats.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="grid grid-cols-4 gap-10 mt-6">
+        <livewire:stats.stat type="User"/>
+        <livewire:stats.stat type="Order"/>
+        <livewire:stats.stat type="Revenue"/>
+    </div>
+</x-app-layout>
